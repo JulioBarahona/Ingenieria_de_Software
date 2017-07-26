d96d701504bcfe5a55ba9a7d5fcaf7fa1712249f
JulioBarahona <julio.fbm96@gmail.com>
2017-07-25 22:31:52 -0600

    proveedor
    addtoy method

--- src/Proveedor.java
+++ src/Proveedor.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
+
 /**
  * Created by Julio on 7/17/2017.
  */
 public class Proveedor {
     private int id_Proveedor;
+    private ArrayList<Juguete> toysManufactured = new ArrayList<Juguete>();
 
+    public int getId_Proveedor() {
+        return id_Proveedor;
+    }
+
+    public void setId_Proveedor(int id_Proveedor) {
+        this.id_Proveedor = id_Proveedor;
+    }
+
+    public ArrayList<Juguete> getToysManufactured() {
+        return toysManufactured;
+    }
+
+    public void setToysManufactured(ArrayList<Juguete> toysManufactured) {
+        this.toysManufactured = toysManufactured;
+    }
+
+    public void addNewToy(){
+
+    }
 }
